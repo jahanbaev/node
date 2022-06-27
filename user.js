65be1440e1eefd7525ef754f8df963287eb6b602
jahanbaev <65107752+jahanbaev@users.noreply.github.com>
2022-06-27 23:56:24 +0500

    Update user.js

--- user.js
+++ user.js
@@ -5,3 +5,21 @@ const dom = (selector) =>{
 return document.querySelectorAll(selector)
     }
 }
+
+//dom("#element").style
+
+async function requestData(type, url = '', data = {}) {
+
+  const response = await fetch(url, {
+    method: type,
+    mode: 'cors', 
+    cache: 'no-cache', 
+    credentials: 'same-origin', 
+    redirect: 'follow', 
+    referrerPolicy: 'no-referrer', 
+    body: JSON.stringify(data) 
+  });
+  return await response;
+}
+
+//requestData('POST','//url.com', { answer: 42 }).then((data) => {console.log(data.json());});
